11c02b24840e8c015ebac3e3ce0ec0c97c1d01b1
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2024-05-02 17:25:01 -0700

    TS - changed js file

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,46 @@
+const articlesFixtures = {
+    oneArticle:
+    [
+      {
+       "id": 1,
+        "title": "The Habit",
+        "url": "https://www.habitburger.com/",
+        "explanation": "Burgers and Fries",
+        "email": "habit@gmail.com",
+        "dateAdded": "2024-05-03T00:12:53.905Z"      
+      }
+    ],
+
+    threeArticles:
+    [
+        {
+            "id": 2,
+            "title": "Cristino's Bakery",
+            "url": "https://www.cristinosbakery.com/",
+            "explanation": "burritos and tacos",
+            "email": "crist@gmail.com",
+            "dateAdded": "2024-05-03T00:12:53.905Z"
+        },
+
+        {
+            "id": 3,
+            "title": "Mcdonalds",
+            "url": "mcD.com/",
+            "explanation": "Burgers",
+            "email": "mcD@gmail.com",
+            "dateAdded": "2024-05-03T00:12:53.905Z"      
+        },
+
+        {
+            "id": 4,
+            "title": "pizza",
+            "url": "https://www.pizza.com/",
+            "explanation": "pizza and Fries",
+            "email": "pizza@gmail.com",
+            "dateAdded": "2024-05-03T00:12:53.905Z"      
+        }
+        
+    ]
+};
+
+export { articlesFixtures };
