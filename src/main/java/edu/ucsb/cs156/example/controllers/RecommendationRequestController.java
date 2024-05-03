8f4c78086fd28db96b4e83a71046189bfecc8d27
Mai Nguyen <mainguyen@169-231-205-254.wireless.ucsb.edu>
2024-05-02 18:14:14 -0700

    MD-migrated java files

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>https://github.com/ucsb-cs156-s24/team02-s24-5pm-5/blob/main/src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java</string>
+</dict>
+</plist>
