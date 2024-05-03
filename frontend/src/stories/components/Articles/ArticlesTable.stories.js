5084e0adce5b1d0e0be9b3f79faf4e8e17b0bc72
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2024-05-02 21:21:50 -0700

    TS - fix typo

--- frontend/src/stories/components/Articles/ArticlesTable.stories.js
+++ frontend/src/stories/components/Articles/ArticlesTable.stories.js
@@ -24,13 +24,13 @@ Empty.args = {
 export const ThreeItemsOrdinaryUser = Template.bind({});
 
 ThreeItemsOrdinaryUser.args = {
-    dates: articlesFixtures.threeDates,
+    dates: articlesFixtures.threeArticles,
     currentUser: currentUserFixtures.userOnly,
 };
 
 export const ThreeItemsAdminUser = Template.bind({});
 ThreeItemsAdminUser.args = {
-    dates: articlesFixtures.threeDates,
+    dates: articlesFixtures.threeArticles,
     currentUser: currentUserFixtures.adminUser,
 }
 
