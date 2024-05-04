392dc09c123128c37ec0a2f07307eb9df494d1e9
William Greenwald <walexgreen@gmail.com>
2024-05-04 14:46:00 -0700

    wg - Created helpRequestFixtures.js

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,42 @@
+const helpRequestFixtures = {
+  oneDate: {
+    id: 1,
+    requesterEmail: "test01@gmail.com",
+    teamId: "team01",
+    tableOrBreakoutRoom: "table",
+    explanation: "Want a room",
+    solved: "true",
+    requestTime: "2022-01-02t12:00:00",
+  },
+  threeDates: [
+    {
+      id: 1,
+      requesterEmail: "test01@gmail.com",
+      teamId: "team01",
+      tableOrBreakoutRoom: "table",
+      explanation: "Want a room",
+      solved: "true",
+      requestTime: "2022-01-02t12:00:00",
+    },
+    {
+      id: 2,
+      requesterEmail: "test02@gmail.com",
+      teamId: "team02",
+      tableOrBreakoutRoom: "table",
+      explanation: "Want a room",
+      solved: "true",
+      requestTime: "2022-01-02t12:00:00",
+    },
+    {
+      id: 3,
+      requesterEmail: "test03@gmail.com",
+      teamId: "team03",
+      tableOrBreakoutRoom: "table",
+      explanation: "Want a room",
+      solved: "true",
+      requestTime: "2022-01-02t12:00:00",
+    },
+  ],
+};
+
+export { helpRequestFixtures };
