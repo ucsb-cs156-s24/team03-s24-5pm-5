00f32d245021dc5fbddb4eba0747ab757c3dc6f4
Riley Chou <riley33chou@gmail.com>
2024-05-06 22:02:57 -0700

    added fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemsFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemsFixtures = {
+  oneDiningCommonsMenuItem: {
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "Baked Pesto Pasta with Chicken",
+    station: "Entree Specials",
+  },
+  threeDiningCommonsMenuItems: [
+    {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Tofu Banh Mi Sandwich (v)",
+      station: "Entree Specials",
+    },
+    {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Chicken Caesar Salad",
+      station: "Entrees",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "ortega",
+      name: "Cream of Broccoli Soup (v)",
+      station: "Greens & Grains",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemsFixtures };
