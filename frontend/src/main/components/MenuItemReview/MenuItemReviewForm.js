5a405c87e870b021083f7ce447c069d4235ce4a1
Tejas Vissapragada <tejas.vissa@gmail.com>
2024-05-06 21:18:32 -0700

    creating branch

---
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -0,0 +1,64 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function MenuItemReviewForm({ initialContents, submitAction, buttonLabel = "Create" }){
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+
+    const navigate = useNavigate();
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+            
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="id">Id</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-id"}
+                        id="id"
+                        type="text"
+                        {...register("id")}
+                        value={initialContents.id}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="itemId">itemId</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-itemId"}
+                    id="itemId"
+                    type="text"
+                    isInvalid={Boolean(errors.itemId)}
+                    {...register("itemId", {
+                        required: "itemId is required.",
+                        maxLength : {
+                            value: 30,
+                            message: "Max length 30 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+
+
+
+
+        </Form>
+    )
+}
+
+export default MenuItemReviewForm;
